347aedc6a82c3d1754668c9b3ac0d8f5826835f2
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-11-20 11:15:36 +0700

    Checkpoint, register login done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:kickstash/screens/menu.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:kickstash/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,15 +11,21 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'KickStash',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.grey,
-        ).copyWith(secondary: Colors.grey[800]),
-        useMaterial3: true,
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'KickStash',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.grey,
+          ).copyWith(secondary: Colors.grey[800]),
+          useMaterial3: true,
+        ),
+        home: const LoginPage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
